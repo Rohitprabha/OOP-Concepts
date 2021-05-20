43ccf2e03372d5b6ced5a0f0d0dfcdd6d49e5f44
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-20 20:51:43 +0530

    Add files via upload

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    public class DemoEncap
+    {
+        private String studentName;             // private variables declared these can only be accessed by public methods of class
+        private int studentAge;
+        public String Name
+        {
+
+            get
+            {
+                return studentName;
+            }
+
+            set
+            {
+                studentName = value;
+            }
+
+        }
+        public int Age
+        {
+
+            get
+            {
+                return studentAge;
+            }
+
+            set
+            {
+                studentAge = value;
+            }
+
+        }
+
+
+    } 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoEncap obj = new DemoEncap();
+            obj.Name = "Ram";
+            obj.Age = 23;
+            Console.WriteLine("Name: " + obj.Name);
+            Console.WriteLine("Age: " + obj.Age);
+        }
+    }
+}
