0933115a26f60a57d33b71db8c4f32c1ce45fccd
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-20 21:01:18 +0530

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Abstract_Class
+{
+    abstract class Animal
+    {
+        public abstract void animalSound();         // Abstract method (does not have a body)
+        public void sleep()                         // Regular method
+        {
+            Console.WriteLine("boww");
+        }
+    }
+    class Dog : Animal                                 // Derived class (inherit from Animal)
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The Dog says: boww boww");         // The body of animalSound() is provided here
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal a = new Animal();                    //cannot create an object for abstract class
+            Dog myDog = new Dog();
+            myDog.animalSound();
+            myDog.sleep();
+        }
+    }
+}
